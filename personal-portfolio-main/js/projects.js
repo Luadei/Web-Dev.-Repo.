65b18f90cdbb3ec3edbc6d797223df06972eafd3
Luadei <108536218+Luadei@users.noreply.github.com>
2024-02-04 11:50:58 +0100

    Add files via upload

---
+++ personal-portfolio-main/js/projects.js
@@ -0,0 +1,29 @@
+const projects = [
+	{
+		title: "Restful API Livres",
+		description: "Une API pour gérer une liste de livres",
+		imageUrl: "flask-api.png",
+		technologies: ["Python", "Restful API"],
+		links: [
+			"https://github.com/Luadei/Dev.-Web-Repo./tree/Rest_api_livres",
+		],
+	},
+	{
+		title: "Bootstrap5 Landpage",
+		description: "l'idée était de s'en inspirer et de refaire le site de mon entreprise",
+		imageUrl: "front-01.png",
+		technologies: ["Html", "Css"],
+		links: [
+			"https://github.com/Luadei/Dev.-Web-Repo./tree/Basic_html_landpage",
+		],
+	},
+	{
+		title: "Bootstrap5 Game Manager",
+		description: "Une page avec une liste de differents jeux avec Bootstrap",
+		imageUrl: "game-manager.png",
+		technologies: ["Bootstrap 5", "Javascript"],
+		links: [
+			"https://github.com/Luadei/Dev.-Web-Repo./tree/Bootstrap5_game_manager",
+		],
+	},
+]
